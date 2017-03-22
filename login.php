5b5e9ae3a314a718efbdd37e9ff412eb57044f8f
arenalg <arenalmig@gmail.com>
2017-03-22 09:48:38 +0100

    Update login.php

--- login.php
+++ login.php
@@ -4,8 +4,8 @@ $db_username = "";
 $db_password = "";
 $db_name = "clinica_upm";
 
-$DNI = $_POST['DNI'];
-$password = $_POST['password']
+$DNI = $_POST['usr'];
+$password = $_POST['passwd']
 
 $conn = new mysql_connect($servername, $db_username, $db_password) or die('No se pudo conectar a la base de datos: ' . mysql_error());;
 
